1802afe65feedd5720f4b17668e05355b4a45e96
Avi <avinashshetty2004@gmail.com>
2026-02-01 17:59:07 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/navbar/navbar.jsx'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+     <Outlet/>
+    </>
+  )
+}
+
+export default App
